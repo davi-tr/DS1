f0a8ee89d9741bc218f5a2ffd3d1b8b306de8ba1
Davi Trajano <davitrajano473@gmail.com>
2023-08-02 20:23:42 -0300

    Projeto spring iniciado

---
+++ src/main/java/br/com/femass/ProjetoDS1/ProjetoDs1Application.java
@@ -0,0 +1,13 @@
+package br.com.femass.ProjetoDS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDs1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/femass/ProjetoDS1/ProjetoDs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.femass.ProjetoDS1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

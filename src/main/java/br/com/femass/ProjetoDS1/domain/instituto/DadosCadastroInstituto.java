0cf6ad80bc3e4b5c69d52cd7ee02c1e39b42a937
wallacebarcelos <wallacebarcelos5@gmail.com>
2023-08-21 15:05:25 -0300

    Comentarios

--- src/main/java/br/com/femass/ProjetoDS1/domain/instituto/DadosCadastroInstituto.java
+++ src/main/java/br/com/femass/ProjetoDS1/domain/instituto/DadosCadastroInstituto.java
@@ -1,12 +1,25 @@
+// Esta é uma classe de registro (record) que define os dados usados para cadastrar um novo Instituto.
+// Ela contém informações como o nome e acrônimo do Instituto, que são necessários para o cadastro.
+
 package br.com.femass.ProjetoDS1.domain.instituto;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-// @notblank uma String restrita é válida desde que não seja nula e o comprimento aparado seja maior que zero.
+
+// Definição da classe de registro (record) DadosCadastroInstituto.
 public record DadosCadastroInstituto(
+        // Anotação "@NotBlank" indica que o campo "nome" não pode estar em branco (nulo ou vazio).
         @NotBlank
         String nome,
+        // Anotação "@NotBlank" indica que o campo "acronimo" não pode estar em branco (nulo ou vazio).
         @NotBlank
         String acronimo
 )
 {
+    // Esta classe de registro (record) é usada para definir os dados necessários para o cadastro de um Instituto.
+
+    // A anotação "@NotBlank" é usada para validar que os campos "nome" e "acronimo" não podem estar em branco,
+    // ou seja, eles devem ter conteúdo e não podem ser nulos ou vazios.
+
+    // Essa classe é uma forma conveniente de definir uma estrutura de dados imutável para a criação de novos Institutos.
 }
--- src/main/java/br/com/femass/ProjetoDS1/domain/instituto/DadosListagemInstituto.java
+++ src/main/java/br/com/femass/ProjetoDS1/domain/instituto/DadosListagemInstituto.java
@@ -1,8 +1,13 @@
+// Esta é uma classe de registro (record) que define os dados usados para listar informações resumidas de um Instituto.
+// Ela contém informações como o ID, nome e acrônimo do Instituto, que são usados na listagem.
+
 package br.com.femass.ProjetoDS1.domain.instituto;
 
-//maneira que dados são recebidos
+// Definição da classe de registro (record) DadosListagemInstituto.
 public record DadosListagemInstituto(Long id, String nome, String acronimo) {
+    // Construtor que recebe um objeto do tipo Instituto para criar uma instância de DadosListagemInstituto.
     public DadosListagemInstituto(Instituto instituto) {
+        // Usa os métodos de acesso do Instituto para obter o ID, nome e acrônimo e inicializa os campos do registro.
         this(instituto.getId(), instituto.getNome(), instituto.getAcronimo());
     }
 }
